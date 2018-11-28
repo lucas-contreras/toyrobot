b18476695905631f87d55baeb1f8ed36cb82c8f0
Lucas M Contreras <Lucas.M.Contreras@gds.ey.com>
2018-11-28 14:21:41 -0300

    initial commits

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = function(env, args) {
+	return {
+		mode: "development",
+		entry: path.join(__dirname, "./index.js"),
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					loader: "babel-loader"
+				},
+				{
+					test: /\.(css|less)$/,
+					loaders: ["style-loader", "css-loader"]
+				}
+			]
+		},
+		devtool: "source-map",
+		target: "web",
+		resolve: {
+			extensions: [".js", ".jsx", ".css"]
+		},
+		output: {
+			path: __dirname + "/dist",
+			filename: "bundle.js"
+		},
+		devServer: {
+			publicPath: "/dist/",
+			port: 8080
+		}
+	};
+};
